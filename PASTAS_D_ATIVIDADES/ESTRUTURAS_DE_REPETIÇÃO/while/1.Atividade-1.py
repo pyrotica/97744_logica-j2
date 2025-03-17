2d27a9d0fc53d0babbbc5bb7e734706eae27fcb1
Miguel <perfectact.2023@gmail.com>
2025-03-17 09:57:23 +0000

    salvar

---
+++ PASTAS_D_ATIVIDADES/ESTRUTURAS_DE_REPETIÇÃO/while/1.Atividade-1.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls || clear")
+
+
+while True:
+    num=int(input("digite um numero positivo: "))
+
+    if num <0:
+        print("Numero negativo inserido, codigo encerrado")
+        break
+    else:
+        print("numero positivo")
