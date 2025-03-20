4ae539c8ccda2050f81d61c52a57801143ede4e9
Miguel <perfectact.2023@gmail.com>
2025-03-20 19:25:11 +0000

    salvar

--- PASTAS_D_ATIVIDADES/ESTRUTURAS_DE_REPETIÇÃO/while/10.Atividade-10 copy.py
+++ PASTAS_D_ATIVIDADES/ESTRUTURAS_DE_REPETIÇÃO/while/10.Atividade-10 copy.py
@@ -1,4 +1,4 @@
-#com while
+#com for
 
 #limpar o craxe
 import os
@@ -7,9 +7,8 @@ os.system("cls || clear")
 #dados pre colocados
 senha_cadastrada="1234"
 login_cadastrado="migues"
-contador = 0
 
-while True:
+for i in range(3):
     
      login=str(input("digite o usuario: ")).lower()
      senha=str(input("digite a senha: "))
@@ -17,17 +16,15 @@ while True:
      if senha==senha_cadastrada and login==login_cadastrado:
 #caso o login e senha sejam corretos
         print()
-        print("login e senha corretas \nseja bem-vindo")
+        print("login e senha corretas, seja bem-vindo")
         break
-     
+
 #caso ultrapasse o numero de tentativas
      else:
         print()
         print("login ou senha incorretos")
         print()
-        contador +=1
-     if contador==3: 
+     if  i==2: 
       print("TENTATIVAS DEMAIS, COMPUTADOR BLOQUEADO")
-      break
 
 
